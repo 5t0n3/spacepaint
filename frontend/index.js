decaaba798759d1781631ec16637c9ed28fcd3c8
Max <35409580+blahblahbloopster@users.noreply.github.com>
2024-12-04 20:10:48 -0800

    marginally better map

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+var map = {};
+
+window.addEventListener('DOMContentLoaded', function () {
+    map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        drawControl: true
+    }).addTo(map);
+});
+
+
