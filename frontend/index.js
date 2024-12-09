85233a5bb7d102dc0fb7fa4c0909b451e8bda11d
Max <35409580+blahblahbloopster@users.noreply.github.com>
2024-12-08 21:23:17 -0800

    integrate rust with javascript code

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,5 @@
+import init, { Rect, LatLong, Pixel, update_viewport, do_changes, rect, latlong } from "./png-decoder/pkg/png_decoder.js";
+
 var map = {};
 var mode = 'none';
 let objects = [];
@@ -100,6 +102,51 @@ function marchingSquares(field, threshold,location,zoom) {
 
 var bar;
 
+//indexed "yx"
+let polygons=[]
+let Polygons=[]
+
+function update_map(data, width, area) {
+    for (P of Polygons) {
+        P.remove(map);
+    }
+    Polygons=[]
+    let array = [];
+    let location=[];
+    let Zoomlist=[20,16,9,6,4,1.5,1,0.5,0.2,0.1,0.05,0.03,0.02,0.01,0.005];
+    let zoom=Zoomlist[map.getZoom()];
+    let height_px = data.length;
+    //send page stuff
+    let y_idx = 0;
+    for (let y = area.bottom_right.lat; y < area.top_left.lat; y += (area.top_left.lat - area.bottom_right.lat) / height_px) {
+        let row = [];
+        let xrow = [];
+        let x_idx = 0;
+        for (let x = area.top_left.long; x < area.bottom_right.long; x += (area.top_left.long - area.bottom_right.long) / width) {
+            row.push(data[y_idx][x_idx++]);
+            //get value (prolly outside of loop)
+            xrow.push([x,y]);
+        }
+        y_idx++;
+        array.push(row);
+        location.push(xrow);
+    }
+    console.log(map.getCenter().lat,"lat")
+    console.log(map.getCenter().lng,"lng")
+    console.log(map.getZoom(),"zoom")
+    
+    for (let v = 0; v < 255; v += 255 / 10) {
+        //console.log(Polygons)
+        polygons = marchingSquares(array, v,location,zoom);
+        for (p of polygons) {
+            P = L.polygon(p, { color: "#aa00ff", fillOpacity: 0.1, stroke: false });
+            P.addTo(map);
+            //console.log(P);
+            Polygons.push(P);
+        }
+    }
+}
+
 window.addEventListener('DOMContentLoaded', function () {
     map = L.map('map').setView([10, 10], 5);
 
@@ -115,16 +162,18 @@ window.addEventListener('DOMContentLoaded', function () {
     
     map.on('click', function() {
         paintMode = !paintMode;
-      if (paintMode) {
-          myPolyline = L.polyline([]).addTo(map);
-      }else{
-        myPolyline.remove(map)
-        let coords=myPolyline.getLatLngs()
-        for (coord of coords){
-            console.log(coord)
+          if (paintMode) {
+              myPolyline = L.polyline([]).addTo(map);
+          } else {
+              myPolyline.remove(map)
+              let coords = myPolyline.getLatLngs()
+              let points = [];
+              for (coord of coords){
+                  points.push(latlong(coord.lat, coord.lng));
+              }
+
+              do_changes(points, brush_size, mode);
         }
-        //send coords of drawing
-    }
     })
     
     map.on('mousemove', function(e) {
@@ -139,45 +188,11 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     })
 
-    //indexed "yx"
-    let polygons=[]
-    let Polygons=[]
     map.on('move', function() {
-        for (P of Polygons) {
-            P.remove(map);
-        }
-        Polygons=[]
-        let array = [];
-        let location=[];
-        let Zoomlist=[20,16,9,6,4,1.5,1,0.5,0.2,0.1,0.05,0.03,0.02,0.01,0.005];
-        let zoom=Zoomlist[map.getZoom()];
-        //send page stuff
-        for (let y = map.getCenter().lng-20*zoom; y < 20*zoom+map.getCenter().lng; y+=zoom) {
-            let row = [];
-            let xrow = [];
-            for (let x = map.getCenter().lat-10*zoom; x < 12*zoom+map.getCenter().lat; x+=zoom) {
-                row.push(Math.sin(x) * Math.cos(y)+0.5*Math.sin(100*x) * Math.cos(100*y));
-                //get value (prolly outside of loop)
-                xrow.push([x,y]);
-            }
-            array.push(row);
-            location.push(xrow);
-        }    
-	    console.log(map.getCenter().lat,"lat")
-        console.log(map.getCenter().lng,"lng")
-        console.log(map.getZoom(),"zoom")
-        
-        for (let v = -1; v < 1; v += 1 / 5) {
-            //console.log(Polygons)
-            polygons = marchingSquares(array, v,location,zoom);
-            for (p of polygons) {
-                P=L.polygon(p, { color: "#aa00ff", fillOpacity: 0.1, stroke: false });
-                P.addTo(map);
-                //console.log(P);
-                Polygons.push(P);
-            }
-        }        
-    })
+        let bounds = map.getBounds();
+        console.log(bounds);
+        update_viewport(rect(bounds.getNorth(), bounds.getWest(), bounds.getSouth(), bounds.getEast()));
+    });
 
 
     var action1 = L.Toolbar2.Action.extend({
@@ -199,5 +214,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }).addTo(map);
 
     console.log(bar.options.actions);
+
+    init();
 });
 
